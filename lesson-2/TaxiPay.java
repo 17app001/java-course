eb4e9d812c7aa27c2a25fca6964b0719ebba6e09
jerry <jerry@gmail.com>
2022-05-17 16:17:44 +0800

    lesson-2

---
+++ lesson-2/TaxiPay.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class TaxiPay {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("請輸入公里數:");
+        float km = input.nextFloat();
+
+        int pay = (int) ((km - 1) * 20 + 70);
+
+        System.out.printf("里程數為:%.2f\t費用為:%d元(原始費用:%.2f)", km, pay,
+                (km - 1) * 20 + 70);
+        input.close();
+    }
+}
