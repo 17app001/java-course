f13dec6d167a97249b3c851ce68f000ac527c041
jerry <jerry@gmail.com>
2022-05-20 16:50:47 +0800

    chapter4-1

---
+++ lesson-4/Ch4_1.java
@@ -0,0 +1,10 @@
+public class Ch4_1 {
+    public static void main(String[] args) {
+        int a = 10, b = 5;
+        if (a > b) {
+            System.out.println("a>b");
+        } else {
+            System.out.println("a<b");
+        }
+    }
+}
---
+++ lesson-4/Ch4_2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Ch4_2 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("請輸入分數:");
+        int score = input.nextInt();
+
+        if (score == 100) {
+            System.out.println("A+");
+        } else if (score >= 90) {
+            System.out.println("A");
+        } else {
+            System.out.println("E");
+        }
+
+        input.close();
+    }
+}
