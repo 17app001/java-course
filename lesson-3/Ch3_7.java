c12547181a73d15c02ec1bdf9a069253706957f8
jerry <jerry@gmail.com>
2022-05-31 14:58:02 +0800

    chapter-6

--- lesson-3/Ch3_7.java
+++ lesson-3/Ch3_7.java
@@ -5,8 +5,8 @@ public class Ch3_7 {
                         56 > (-10 + 30) + 5 * 10);
 
         System.out.println(-10 + (38 + 20) * 5 / 10);
-        System.out.println(50 > 100 || !true && 99 > 50);
-        System.out.println(-10 > 65 && 33 > (33 % 20 / 10) / 2);
+        // System.out.println(50 > 100 || !true && 99 > 50);
+        // System.out.println(-10 > 65 && 33 > (33 % 20 / 10) / 2);
 
         int a = 10;
         System.out.println(a << 1);
